a1431a2c9d8200a879767507d7dc6fd7770a5b5e
TR559 <uzy5@fresnou.org>
2023-03-15 14:18:50 -0700

    initial comit

---
+++ Functions1.js
@@ -0,0 +1,40 @@
+// function findAreaofRectangle(length, width){
+//     console.log(length * width)
+// }
+// findAreaofRectangle(12,4)
+// findAreaofRectangle(54,7)
+
+// function findAreaofCircle (radius ){
+//     console.log(radius * radius * 3.14)
+// }
+// findAreaofCircle (1)
+
+// function findMin(x, y){
+//
+//
+//     if (x<y){
+//
+//         console.log(`The smaller number is ${x}`);
+//
+//     } else {
+//
+//         console.log(`the smaller number is ${y}`);
+//
+//     }
+//
+// }
+// findMin(4,6)
+
+// function checkEquality (x,y){
+//
+//     if (x==y){
+//     console.log('the numbers are equal')
+// }
+//     else {
+//      console.log('numbers not equal')
+//  }
+// }
+// checkEquality(2,2)
+// checkEquality(4,8)
+
+function calculateLargestRectangle (lenght,width)
