33f1091d99f39f585f4e9062d522a67f5c23f9c3
TR559 <uzy5@fresnou.org>
2023-03-24 14:20:19 -0700

    initial comit

--- Araynotes1.js
+++ Araynotes1.js
@@ -1,5 +1,18 @@
 function firstelement(arr){
-console.log(arr [0]);
-}
+    if (arr.length ==0 ){
+        console.log("Empty array");
+    }else {
+        console.log(arr[0])
+    }
+//
+// }
 let myNums = [1,2,3,4];
-firstelement(myNums);
+//
+//  firstelement(myNums);
+// console.log(myNums[3])
+//
+
+function x(arr){
+    for(let ;=arr.length-1; i>=0;vi--);
+console.log(arr[i])
+}
