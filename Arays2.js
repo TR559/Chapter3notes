fc0f27d4134a735f54d96a38a778b4dff527f0c9
TR559 <uzy5@fresnou.org>
2023-03-28 14:18:45 -0700

    initial comit

---
+++ Arays2.js
@@ -0,0 +1,23 @@
+// function count (n){
+//     for( let i = 1; i<=n; i++){
+//         console.log(i)
+//     }
+// }
+// count(6);
+
+// function count (n){
+//     for( let i = 1; i<=n; i++){
+//         console.log(i)
+//     }
+//     if (n<=0){
+//         console.log('Invalid')
+//     }
+//
+// }
+// count(0)
+// count(-3)
+// count(23)
+
+let n = [1,2,3,4,5]{
+    for( )
+}
