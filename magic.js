c314b28324e9dc04c93f97644ac88a9637b0de44
TR559 <uzy5@fresnou.org>
2023-04-14 14:09:45 -0700

    initial comit

--- magic.js
+++ magic.js
@@ -1,4 +1,9 @@
-let answers=['yes','no','maybe','Try agian','idk about that one','joe mama','couldnt hear you try agian '];
+const readLine=require('readline-sync');
+let words=[]
+
+let word=readLine.question(" whats your question:")
+words.push(word)
+let answers=['yes','no','maybe','Try agian','idk about that one','joe mama'];
 
 let randomNum = Math.floor(Math.random()*answers.length);
 let answer = answers[randomNum];
