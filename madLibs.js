5140278e2f782b78b8134de2cf866e6eca09c6f8
TR559 <uzy5@fresnou.org>
2023-04-17 14:00:22 -0700

    initial comit

--- madLibs.js
+++ madLibs.js
@@ -1,5 +1,5 @@
-const readLine=require('readline-sync');
-let words=[]
+const readLine = require('readline-sync');
+let words = []
 
 let word=readLine.question("noun:")
 words.push(word)
@@ -35,40 +35,42 @@ let word10=readLine.question("noun: ")
 words.push(word10)
 
 let word11=readLine.question("adjective: ")
-words.push(word12)
+words.push(word11)
 
 let word12=readLine.question("adjective: ")
-words.push(word13)
+words.push(word12)
 
 let word13=readLine.question("adjective: ")
-words.push(word14)
+words.push(word13)
 
 let word14=readLine.question("noun: ")
-words.push(word15)
+words.push(word14)
 
 let word15=readLine.question("noun: ")
-words.push(word16)
+words.push(word15)
 
 let word16=readLine.question("noun: ")
-words.push(word17)
+words.push(word16)
 
 let word17=readLine.question("noun: ")
-words.push(word18)
+words.push(word17)
 
 let word18=readLine.question("adjective: ")
-words.push(word19)
+words.push(word18)
 
 let word19=readLine.question("noun: ")
-words.push(word20)
+words.push(word19)
 
 let word20=readLine.question("verb: ")
-words.push(word21)
+words.push(word20)
 
 let word21=readLine.question("noun: ")
-words.push(word22)
+words.push(word21)
 
 let word23=readLine.question("adjective: ")
 words.push(word23)
 
-let word24=readLine.question("noun: ")
-words.push(word24)
+
+
+console.log(`There were two friends who lived in a ${word[0]} near Jaipur called ${word[1]} and ${word[2]}, and they were friends since childhood and were studying at a ${word[3]} with each other, and this ${word[4]} was far from their place of residence, and they had to cross ${word[5]} and ${word[6]} and pass over ${word[7]}areas to be able to go to ${word[8]} together. One rainy day. Jay and Vijay had to walk to ${word[9]} as usual, and on their way they were discussing what they had learned about atomic theory in ${word[10]}. They differed in their views, which led to sharp discussions between them that reached the use of vulgar  language. 
+ In a fit of anger, ${word[11]} slapped ${word[12]}, then ${word[13]} was shocked by his friend and got angry and wrote on the ${word[14]}, "Today my best friend slapped me." Then they both continued walking towards the ${word[15]} in clear silence. On their way they reached the ${word[16]} which was ${word[17]} and they had to cross it, ${word[18]} walked towards the ${word[19]} knowing that he could not swim, he started to ${word[20]}, and with the force of the ${word[21]} flowing and the ${word[22]} he felt as if the ${word[23]} would take him forever.  `);
