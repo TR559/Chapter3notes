780f4cb221b6df64262771477908c3a1554e012d
TR559 <uzy5@fresnou.org>
2023-04-17 14:13:51 -0700

    initial comit

--- magic.js
+++ magic.js
@@ -1,6 +1,6 @@
 const readLine=require('readline-sync');
 let words=[]
-
+console.log( 'Ask any Question i will Answer to my best abilities.');
 let word=readLine.question(" whats your question:")
 words.push(word)
 let answers=['yes','no','maybe','Try agian','idk about that one','joe mama'];
