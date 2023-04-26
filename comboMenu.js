e9f697acff2651e978a7504af96f628197e5e3b9
TR559 <uzy5@fresnou.org>
2023-04-26 14:41:52 -0700

    initial comit

--- comboMenu.js
+++ comboMenu.js
@@ -2,8 +2,9 @@ const readLine=require('readline-sync');
     let words=[]
 
          console.log( 'Lunch Menu');
+            let sandwiches = [ 'chicken $5.25', 'beef $6.25', 'tofu $5.75', 'ham $4.75' , 'turkey $6.25' ]
 
-                console.log( "chicken $5.25, beef $6.25, tofu $5.75, ham $4.75 , turkey $6.25 ")
+                console.log(sandwiches)
 
     let word=readLine.question("What type of sandwich would you like:")
 
@@ -14,7 +15,10 @@ const readLine=require('readline-sync');
     let word1=readLine.question("Would you like a drink?:")
 
             if(word1 == "yes"){
-                console.log(" small $1.00 , medium $1.75 , large $2.25 ");
+
+                let drinks = [ 'small $1.00' , 'medium $1.75' , 'large $2.25' ]
+
+                console.log(drinks)
 
                 let word2 =readLine.question("Witch size drink would you like?:")
 
